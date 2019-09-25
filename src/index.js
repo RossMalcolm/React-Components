9fb1dadec6935a39260f570d74076f4ca09bc1e6
Ross Malcolm <rossmalc75@gmail.com>
2019-09-25 12:13:43 -0400

    Approval Card

--- src/index.js
+++ src/index.js
@@ -2,28 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () =>{
     return (
         <div className = "ui container comments">
-            <CommentDetail
-            author = "Ross" 
-            timeAgo = "Today at 5:00PM" 
-            avatar = {faker.image.avatar()} 
-            text = "nice" 
+            <ApprovalCard>
+                <CommentDetail
+                author = "Ross" 
+                timeAgo = "Today at 5:00PM" 
+                avatar = {faker.image.avatar()} 
+                text = "nice" 
+                />
+            </ApprovalCard> 
+            <ApprovalCard>  
+                <CommentDetail 
+                author = "Isaac" 
+                timeAgo = "Today at 2:23PM" 
+                avatar = {faker.image.avatar()} 
+                text = "nice" 
             />
-            <CommentDetail 
-            author = "Isaac" 
-            timeAgo = "Today at 2:23PM" 
-            avatar = {faker.image.avatar()} 
-            text = "nice" 
-            />
-            <CommentDetail 
-            author = "Brendan" 
-            timeAgo = "Yesterday at 9:00AM" 
-            avatar = {faker.image.avatar()} 
-            text = "nice" 
-            />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                author = "Brendan" 
+                timeAgo = "Yesterday at 9:00AM" 
+                avatar = {faker.image.avatar()} 
+                text = "nice" 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                author = "Rebecca" 
+                timeAgo = "Yesterday at 9:00AM" 
+                avatar = {faker.image.avatar()} 
+                text = "Yeet" 
+                />
+            </ApprovalCard>
         </div>
     );
 };
